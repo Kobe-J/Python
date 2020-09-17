a2bc229f7000afadf03a01d27a1b4b4db6fb539c
Kobe-J <2215557239@qq.com>
2020-09-17 16:05:13 +0800

    2020.9.17

--- seleniumtest1/stu1.py
+++ seleniumtest1/stu1.py
@@ -300,3 +300,15 @@ import math
 # print(sum(range(1, 5)))
 
 
+mylist = [x for x in range(1, 100)]
+
+
+mylist1 = [x for x in range(1, 100, 2) if x < 5] #嵌套判断
+
+mylist2 = [x * x + x for x in range(1, 101, 2) if x < 50]  #嵌套元素公式
+
+mylist3 = [[x * x] for x in range(1, 100, 2) if x < 50]  #嵌套列表
+
+mylist4 = [x + y for x in range(100) for y in range(100)]  #嵌套循环
+
+print(mylist,"\n",mylist1,"\n",mylist2,"\n",mylist3,"\n",mylist4)
